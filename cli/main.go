628ad97e6eefdb98c6b86658847d78ea01fd361b
Lachlan Donald <lachlan@ljd.cc>
2017-07-12 20:48:51 +1000

    Initial commit

---
+++ cli/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	touchid "github.com/lox/go-touchid"
+)
+
+func main() {
+	ok, err := touchid.Authenticate("access llamas")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if ok {
+		log.Printf("Authenticated")
+	} else {
+		log.Fatal("Failed to authenticate")
+	}
+}
